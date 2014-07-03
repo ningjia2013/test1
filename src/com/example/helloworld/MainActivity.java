9641540db391e80d59079bcca3d89e799f398736
hinar83 <hinar83@126.com>
2014-07-04 05:48:17 +0800

    first modify

--- src/com/example/helloworld/MainActivity.java
+++ src/com/example/helloworld/MainActivity.java
@@ -6,6 +6,8 @@ import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+    private boolean first = true;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
