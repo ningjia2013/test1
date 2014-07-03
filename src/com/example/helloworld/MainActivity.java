c8a6ea0848508a124194cac91b9ca7e21d01ae7f
ningjia2013 <623982146@qq.com>
2014-07-04 05:50:26 +0800

    Update MainActivity.java
    server first modify

--- src/com/example/helloworld/MainActivity.java
+++ src/com/example/helloworld/MainActivity.java
@@ -6,6 +6,8 @@ import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+        private int curTime = 0;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
